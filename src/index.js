e2c36f260b1074f3493bc0740240ceaee1928fa0
shashankmadaan379 <codebreaker379@gmail.com>
2023-08-21 14:28:04 +0530

    Initial Setup Done

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PORT } = require("./config/serverConfig");
+const setUpAndStartServer = () => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log(`Server started at PORT ${PORT}`);
+  });
+};
+setUpAndStartServer();
