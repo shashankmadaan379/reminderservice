c81f53ff7d75da6d496a7a73a373d1b3621e21ee
shashankmadaan379 <codebreaker379@gmail.com>
2023-08-21 15:27:29 +0530

    Setup of SMTP using nodemailer done

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,4 +2,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
   PORT: process.env.PORT,
+  EMAIL_ID: process.env.EMAIL_ID,
+  EMAIL_PASS: process.env.EMAIL_PASS,
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
+const { sendBasicEmail } = require("./services/email-service");
 const setUpAndStartServer = () => {
   const app = express();
   app.use(bodyParser.json());
@@ -8,6 +9,13 @@ const setUpAndStartServer = () => {
 
   app.listen(PORT, () => {
     console.log(`Server started at PORT ${PORT}`);
+    // Send email to myself for testing purposes only!
+    sendBasicEmail(
+      "nodejsreminderservice@gmail.com",
+      "skmadaan11@gmail.com",
+      "This is a testing Email",
+      "<h1>Hello World</h1>"
+    );
   });
 };
 setUpAndStartServer();
