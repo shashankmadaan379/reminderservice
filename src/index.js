4d5fd9af233591287d41189f677350ba1fcd969b
shashankmadaan379 <codebreaker379@gmail.com>
2023-08-22 06:35:48 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const { sendBasicEmail } = require("./services/email-service");
+const cron = require("node-cron");
 const setUpAndStartServer = () => {
   const app = express();
   app.use(bodyParser.json());
@@ -10,12 +11,15 @@ const setUpAndStartServer = () => {
   app.listen(PORT, () => {
     console.log(`Server started at PORT ${PORT}`);
     // Send email to myself for testing purposes only!
-    sendBasicEmail(
-      "nodejsreminderservice@gmail.com",
-      "skmadaan11@gmail.com",
-      "This is a testing Email",
-      "<h1>Hello World</h1>"
-    );
+    // sendBasicEmail(
+    //   "nodejsreminderservice@gmail.com",
+    //   "skmadaan11@gmail.com",
+    //   "This is a testing Email",
+    //   "<h1>Hello World</h1>"
+    // );
+    // cron.schedule("*/2 * * * *", () => {
+    //   console.log("running a task every two minutes");
+    // });
   });
 };
 setUpAndStartServer();
---
+++ src/models/notificationticket.js
@@ -0,0 +1,32 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class NotificationTicket extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  NotificationTicket.init(
+    {
+      subject: { type: DataTypes.STRING, allowNull: false },
+      content: { type: DataTypes.STRING, allowNull: false },
+      status: {
+        type: DataTypes.ENUM,
+        allowNull: false,
+        values: ["PENDING", "SUCCESS", "FAILED"],
+      },
+      recepientEmail: { type: DataTypes.STRING, allowNull: false },
+      notificationTime: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "NotificationTicket",
+    }
+  );
+  return NotificationTicket;
+};
