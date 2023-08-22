c2cd712240388d9f615bf3dcc3c3b3d1760add12
shashankmadaan379 <codebreaker379@gmail.com>
2023-08-22 20:30:48 +0530

    Added Notification Cron

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const { sendBasicEmail } = require("./services/email-service");
 const cron = require("node-cron");
+const jobs = require("./utils/job");
 const apiRoutes = require("./routes/index");
 const setUpAndStartServer = () => {
   const app = express();
@@ -11,6 +12,7 @@ const setUpAndStartServer = () => {
   app.use("/api", apiRoutes);
   app.listen(PORT, () => {
     console.log(`Server started at PORT ${PORT}`);
+    jobs();
     // Send email to myself for testing purposes only!
     // sendBasicEmail(
     //   "nodejsreminderservice@gmail.com",
