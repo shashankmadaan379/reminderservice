4e1f85b3db76ef54cea5985697f314d562a9cb0d
Shashank <shashankmadaan379@gmail.com>
2023-10-02 13:06:23 +0530

    errors resolved

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
-<<<<<<< HEAD
-// const { sendBasicEmail } = require("./services/email-service");
-// const cron = require("node-cron");
-// const jobs = require("./utils/job");
-=======
 const { sendBasicEmail } = require("./services/email-service");
 const cron = require("node-cron");
 const jobs = require("./utils/job");
->>>>>>> c2cd712240388d9f615bf3dcc3c3b3d1760add12
 const apiRoutes = require("./routes/index");
 const setUpAndStartServer = () => {
   const app = express();
@@ -18,11 +12,7 @@ const setUpAndStartServer = () => {
   app.use("/api", apiRoutes);
   app.listen(PORT, () => {
     console.log(`Server started at PORT ${PORT}`);
-<<<<<<< HEAD
-    // jobs();
-=======
     jobs();
->>>>>>> c2cd712240388d9f615bf3dcc3c3b3d1760add12
     // Send email to myself for testing purposes only!
     // sendBasicEmail(
     //   "nodejsreminderservice@gmail.com",
